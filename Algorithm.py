Move bounds, change coci derivative and fix coci bound bugs

Algorithm.py
@@ -1,7 +1,6 @@
-from scipy.optimize import minimize
-from scipy.special import lambertw
 import math
 import numpy as np
+import Bounds as bd
 
 class Algorithm:
     def __init__(self, scenario, bound):
@@ -10,11 +9,17 @@ class Algorithm:
         self.K = scenario.game.size()
         self.subg = scenario.game.subg()
         self.mix = scenario.mix
+        self.startup = 1
         if bound == "lil":
             self.epsilon = 0.01
-            self.delta = calculate_delta_lil(self.alpha, self.K, self.epsilon)
+            self.delta = bd.calculate_delta_lil(self.alpha, self.K, self.epsilon)
+            self.startup = 2
         elif bound == "hoeffding":
-            self.delta = calculate_delta_hoeffding(self.alpha, self.K, self.subg, scenario.W)
+            t_bound_coeff = self.sample_bound_coeff(scenario.W) # t_max depends on specific algorithm TODO tighten bounds
+            self.delta = bd.calculate_delta_hoeffding(t_bound_coeff, scenario.W, self.K, self.alpha)
+        elif bound == "coci": # requires bounded in [0,1]
+            assert(scenario.game.isBounded())
+            self.delta = self.alpha # probability of error is self.alpha
         else:
             assert(False)
 
@@ -25,28 +30,32 @@ class Algorithm:
             return Opt(scenario, bound)
         elif name == "uniform":
             return Uniform(scenario, bound)
+        elif name == "coci":
+            return COCI(scenario, bound)
         else:
             assert(False)
 
     def width(self, means, samples):
-        upper_bounds = means + self.bound_superarms(means, samples)
-        lower_bounds = means - self.bound_superarms(means, samples)
+        sa_means = coefficientize(self.mix, means)        
+        upper_bounds = sa_means + self.bound_superarms(means, samples)
+        lower_bounds = sa_means - self.bound_superarms(means, samples)
         return np.max(upper_bounds) - np.max(lower_bounds)
 
-    def sample(self, means, samples, mix):
+    def sample(self, means, samples):
         pass
 
     def bound_superarms(self, means, samples): # returns bounding term
-        bound_sa = np.zeros((len(means)))
         if self.bound == "lil":
-            bound_single = np.array([lil_bound(self.epsilon, self.delta, t, self.subg) for t in samples])
-            for i in range(len(means)):
-                coeff = coefficient(self.mix, i)
-                bound_sa[i] = np.dot(coeff, bound_single)
+            bound_single = np.array([bd.lil_bound(self.epsilon, self.delta, t, self.subg) for t in samples])
+            bound_sa = coefficientize(self.mix, bound_single)
         elif self.bound == "hoeffding":
+            bound_sa = np.zeros((len(means)))
             for i in range(len(means)):
                 coeff = coefficient(self.mix, i)
-                bound_sa[i] = hoeffding_bound(self.delta, samples, self.subg, coeff)
+                bound_sa[i] = bd.hoeffding_bound(self.delta, samples, self.subg, coeff)
+        elif self.bound == "coci":
+            bound_single = np.array([bd.coci_bound(self.delta, sample, sum(samples), self.startup) for sample in samples])
+            bound_sa = coefficientize(self.mix, bound_single)
         else:
             assert(False)
         return bound_sa
@@ -54,9 +63,11 @@ class Algorithm:
     def derivative_bound(self, means, samples, i_star):
         coeff = coefficient(self.mix, i_star)
         if self.bound == "lil":
-            derivative = np.array([lil_derivative(self.epsilon, self.delta, samples[i], self.subg, coeff[i]) for i in range(self.K)])
+            derivative = np.array([bd.lil_derivative(self.epsilon, self.delta, samples[i], self.subg, coeff[i]) for i in range(self.K)])
         elif self.bound == "hoeffding":
-            derivative = np.array([hoeffding_derivative(coeff[i], samples[i]) for i in range(self.K)])
+            derivative = np.array([bd.hoeffding_derivative(coeff[i], samples[i]) for i in range(self.K)])
+        elif self.bound == "coci":
+            derivative = np.array([bd.coci_derivative(coeff[i], samples[i]) for i in range(self.K)])
         else:
             assert(False)
         return derivative
@@ -71,7 +82,11 @@ class Workshop(Algorithm):
         i_star = np.argmax(bounds)
         derivatives = self.derivative_bound(means, samples, i_star)
         return np.argmax(derivatives)
-        
+
+    # t_max = (8 * subg**2 * log(1/delta) * K)/W**2 
+    def sample_bound_coeff(self, W):
+        return (8 * self.K * self.subg**2) / (W**2)
+
 
 class Opt(Algorithm):
     def __init__(self, scenario, bound):
@@ -83,9 +98,15 @@ class Opt(Algorithm):
         return np.argmax(derivatives)
 
     def width(self, means, samples): # override to use our i_star
-        upper_bounds = means + self.bound_superarms(means, samples)
-        lower_bounds = means - self.bound_superarms(means, samples)
+        sa_means = coefficientize(self.mix, means)
+        upper_bounds = sa_means + self.bound_superarms(means, samples)
+        lower_bounds = sa_means - self.bound_superarms(means, samples)
         return upper_bounds[self.i_star] - lower_bounds[self.i_star]
+
+    # Use same bound as workshop for simplicity
+    # t_max = (8 * subg**2 * log(1/delta) * K)/W**2 
+    def sample_bound_coeff(self, W):
+        return (8 * self.K * self.subg**2) / (W**2)
 
 
 class Uniform(Algorithm):
@@ -95,63 +116,64 @@ class Uniform(Algorithm):
     def sample(self, means, samples):
         return np.argmin(samples)
 
+    # Bound uses t_i = t_j rather than derivative condition
+    # t_max = (16 * subg**2 * log(1/delta) * K)/W**2 
+    def sample_bound_coeff(self, W):
+        return (16 * self.K * self.subg**2) / (W**2)
+
+
+class COCI(Algorithm):
+    def __init__(self, scenario, bound):
+        super(COCI, self).__init__(scenario, bound)
+        assert(scenario.game.isBounded())
+
+    def bound_individual_arms(self, means, samples):
+        if self.bound == "lil":
+            bound_single = np.array([bd.lil_bound(self.epsilon, self.delta, t, self.subg) for t in samples])
+        elif self.bound == "coci": # TODO assumes rewards bounded on [0,1], unclear how to adapt for general subgaussian
+            bound_single = np.array([bd.coci_bound(self.delta, sample, sum(samples), self.startup) for sample in samples])
+        else:
+            assert(False)
+        return bound_single
+
+    def sample(self, means, samples):
+        bounding_terms = self.bound_individual_arms(means, samples)
+        upper_bounds = means + bounding_terms
+        lower_bounds = means - bounding_terms
+        candidates = [] # any arm where we're uncertain about coefficient
+        for i in range(self.K):
+            is_best_arm = all([j == i or lower_bounds[i] > upper_bounds[j] for j in range(len(upper_bounds))]) # we are certain i is best
+            is_not_best_arm = any([j != i and upper_bounds[i] < lower_bounds[j] for j in range(len(upper_bounds))]) # we are certain i is not best
+            if not is_best_arm and not is_not_best_arm: # uncertain
+                candidates.append(i)
+            elif is_best_arm:
+                best_arm = i
+
+        if len(candidates) == 0: # sample optimal distribution for best_arm we know
+            coeff = coefficient(self.mix, best_arm)
+            if self.bound == "lil":
+                derivative = np.array([bd.lil_derivative(self.epsilon, self.delta, samples[i], self.subg, coeff[i]) for i in range(self.K)])
+            elif self.bound == "coci":
+                derivative = np.array([bd.coci_derivative(coeff[i], samples[i]) for i in range(self.K)])
+            else:
+                assert(False)
+            return np.argmax(derivative)
+        else: # sample candidate with largest radius
+            mask = np.ones(len(bounding_terms), dtype=bool)
+            mask[candidates] = False
+            bounding_terms[mask] = 0 # set non-candidate radius to 0
+            return np.argmax(bounding_terms)
+
 
 def coefficient(mix, i_star):
     coeff = np.array(mix)
     coeff[i_star] = 1 - coeff[i_star]
     return coeff
- 
 
-''' LIL Bound '''
-def lil_bound(epsilon, delta, t, subg):
-    t1 = 1 + math.sqrt(epsilon)
-    te = (1 + epsilon) * t
-    t2 = ((2 * (subg**2) * (1 + epsilon)) / t) * math.log(math.log(te) / delta)
-    return t1 * math.sqrt(t2)
-
-def lil_derivative(epsilon, delta, t, subg, coeff):
-    t1 = coeff * (1 + math.sqrt(epsilon)) * math.sqrt(2 * (subg**2) * (1 + epsilon))
-    t2 = 1 / (2 * (t**(3/2)))
-    t3 = math.log(math.log((1 + epsilon) * t) / delta)
-    t4 = t3 - (1 / math.log((1 + epsilon) * t))
-    res = t1 * t2 * t3**(-1/2) * t4
-    if res < 0: # derivative is positive for a while, then becomes negative 
-        print("derivative of bound positive, increase startup sample count")
-        assert(False)
-    return res
-
-def calculate_delta_lil(alpha, K, epsilon):
-    t1 = math.log(1 + epsilon)
-    t2 = (epsilon * alpha) / (2 * K * (2 + epsilon))
-    delta = t1 * (t2**(1 / (1 + epsilon)))
-    print("delta:", delta)
-    return delta
-
-''' Hoeffding Bound '''
-def hoeffding_bound(delta, t, subg, coeff): # t is a vector
-    t1 = 2 * (subg**2) * math.log(1 / delta)
-    t2 = sum([(coeff[i]**2) / t[i] for i in range(len(t))])
-    return math.sqrt(t1 * t2)
-
-# Returns absolute value (positive), with common term removed
-def hoeffding_derivative(c, t): # t and c are scalars for specific term
-    return c**2 / t**2
-
-# Uses the upper bound on t based only on W for all arms on both sides, since we have to union bound over time:
-# t_max = (8 * subg**2 * log(1/delta) * K)/W**2 
-# Constraint on alpha is: alpha = t_max * K * 2 * delta
-# Lambert W function used to solve log(1/delta)*delta = alpha/C.
-def calculate_delta_hoeffding(alpha, K, subg, W):
-    t_bound_coeff = (8 * K * subg**2) / (W**2) # this * log(1/delta) is the upper bound on number samples
-    delta_term_coeff = t_bound_coeff * K * 2 # this * log(1/delta) * delta is the result of union bound over time and arms
-    alpha_term = alpha / delta_term_coeff # this must equal log(1/delta) * delta
-    log_delta = np.real(lambertw(-alpha_term, 1)) # two real solutions, larger delta requires t < 1 so use smaller
-
-    max_t = alpha / (math.exp(log_delta) * 2 * K)
-    assert(max_t >= 20)
-    print("t cannot be more than", max_t)
-
-    delta = math.exp(log_delta)
-    print("delta:", delta)
-
-    return delta
+# Return array of values weighted by coefficients for each position
+def coefficientize(mix, values):
+    sv = np.zeros((len(values)))
+    for i in range(len(values)):
+        coeff = coefficient(mix, i)
+        sv[i] = np.dot(coeff, values)
+    return sv

Scenario.py
@@ -7,27 +7,37 @@ class Scenario:
         W = 0.5
         if name == "uniMeans":
             means = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-            subg = 2
+            std = 2
             mix = [0, 0, 0, 0, 0.25, 0, 0, 0, 0.5, 0.25]
+            self.game = ToyGame(means, std)
         elif name == "closeTop":
             means = [10, 9.5, 4.5, 4, 3.5, 3, 2.5, 2, 1.5, 1]
-            subg = 2
+            std = 2
             mix = [0, 0, 0, 0, 0.25, 0, 0, 0, 0.5, 0.25]
+            self.game = ToyGame(means, std)
         elif name == "bestInSupp":
             means = [10, 9.5, 4.5, 4, 3.5, 3, 2.5, 2, 1.5, 1]
-            subg = 2
+            std = 2
             mix = [0.75, 0, 0, 0, 0.25, 0, 0, 0, 0, 0]
+            self.game = ToyGame(means, std)
         elif name == "fullSupp":
             means = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-            subg = 2
+            std = 2
             mix = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+            self.game = ToyGame(means, std)
         elif name == "closerTop":
             means = [10, 9.9, 4.5, 4, 3.5, 3, 2.5, 2, 1.5, 1]
-            subg = 2
+            std = 2
             mix = [0, 0, 0, 0, 0.25, 0, 0, 0, 0.5, 0.25]
+            self.game = ToyGame(means, std)
+        elif name == "bounded": 
+            W = 0.05
+            means = [0.75, 0.7, 0.65, 0.6, 0.55, 0.45, 0.4, 0.35, 0.3, 0.25]
+            std = 1/4
+            mix = [0, 0, 0, 0, 0.25, 0, 0, 0, 0.5, 0.25]
+            self.game = BoundedGame(means, std)
         else:
             assert(False)
-        self.game = ToyGame(means, subg)
         self.mix = mix
         self.W = W
 
@@ -54,5 +64,24 @@ class ToyGame:
     def subg(self):
         return self.std
 
+    def isBounded(self):
+        return False
 
+# As above, but payoffs bounded to [0, 1].
+# When using BoundedGame, algorithms will only know of the boundedness in [0,1], not the true std. Therefore, self.subg = 1/4, since subg of a bounded variable is (b-a)^2 / 4
+class BoundedGame(ToyGame):
+    def __init__(self, means, std):
+        super(BoundedGame, self).__init__(means, std)
+        assert(all([mean < 1 and mean > 0 for mean in means]))
+        self.min = 0
+        self.max = 1
+
+    def sample(self, strat): # returns a payoff of the deviation payoff for strategy strat
+        return np.clip(np.random.normal(self.means[strat], self.std), self.min, self.max)
+
+    def subg(self):
+        return (self.max - self.min)**2 / 4
+
+    def isBounded(self):
+        return True
 

regret_new.py
@@ -12,15 +12,15 @@ scenario_name: one of the scenarios specified in Scenario.py
 def main(algo_name, bound_name, scenario_name):
     scenario = Scenario(scenario_name)
     algo = Algorithm.make(algo_name, bound_name, scenario)
+    print("Running scenario", scenario_name, "with algorithm", algo_name, "and bound", bound_name)
 
     w = float("inf")
     K = scenario.game.size()
     means = np.zeros((K))
     samples = np.zeros((K))
-    startup = 2
     for i in range(K):
-         samples[i] = startup 
-         for t in range(startup):
+         samples[i] = algo.startup 
+         for t in range(algo.startup):
              means[i] += scenario.game.sample(i)
          means[i] /= samples[i] 
     while algo.width(means, samples) > scenario.W:
@@ -30,6 +30,8 @@ def main(algo_name, bound_name, scenario_name):
         samples[j] += 1
     print("samples taken:", sum(samples))
     print("final bounds:", means + algo.bound_superarms(means, samples))
+    print("samples:", samples)
+    print("widths:", algo.bound_superarms(means, samples))
     print("true means:", scenario.game.means)
 
 if __name__ == '__main__':